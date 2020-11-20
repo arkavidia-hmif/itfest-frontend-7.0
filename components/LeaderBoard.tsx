895c463008ff63cd483dbf41692143cf062f03ee
Akromi Afif <socakromiafif@gmail.com>
2020-11-20 13:59:27 +0700

    add leaderboard

---
+++ components/LeaderBoard.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import LeaderBoardChild from "./LeaderBoardChild";
+import ColorfulHeader from "./ColorfulHeader";
+import { Theme } from "styles/theme";
+
+const LeaderBoard: React.FC = () => {
+  return (
+    <div className="leaderboard">
+      <div className="header">
+        <ColorfulHeader headingLevel={1} color={Theme.headerColors.pipi} size="1em">LEADERBOARD</ColorfulHeader>
+        <p className="visitor">Visitors: 135182</p>
+      </div>
+      <LeaderBoardChild no={1} name="John Doe" score={10000000} />
+      <LeaderBoardChild no={2} name="Jane Doe" score={5000000} />
+      <LeaderBoardChild no={3} name="John Mayer" score={12000000} />
+      <LeaderBoardChild no={4} name="Afif Akromi" score={7000000} />
+
+      <style jsx>{`
+        .leaderboard {
+          width: 50%;
+          margin: 0 auto;
+          display: flex;
+          flex-direction: column;
+          justify-content: space-around;
+          height: 40vh;
+          text-align: center;
+        }
+
+        .visitor {
+          background-size: 100%;
+          background-image: ${Theme.headerColors.pipi};
+          background-clip: text;
+          -webkit-background-clip: text;
+          text-fill-color: transparent;
+          -webkit-text-fill-color: transparent;
+        }
+
+        p {
+          font-weight: bold;
+        }
+      `}</style>
+    </div>
+  )
+}
+
+export default LeaderBoard;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
 import Layout from "components/commons/Layout";
+import LeaderBoard from "../components/LeaderBoard";
 
 const Home: React.FC = () => {
-  return <Layout title="Home">rgt</Layout>;
+  return (
+    <Layout title="Home">
+      <LeaderBoard />
+    </Layout>
+  );
 };
 
 export default Home;
