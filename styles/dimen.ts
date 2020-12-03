b5d2518171294e781201ce528fd3fdc37b0620b5
Jones Napoleon Autumn <13518086@std.stei.itb.ac.id>
2020-12-03 21:48:04 +0700

    add dimen constant

--- styles/dimen.ts
+++ styles/dimen.ts
@@ -2,4 +2,8 @@ export const Dimen = {
   navbarHeight: "85px",
   navbarMobileHeight: "75px",
   navbarBreakpoint: "767px",
+  smBreakpoint: "567px",
+  mdBreakpoint: "767px",
+  xsBreakpoint: "992px",
+  lgBreakpoint: "1200px",
 };
