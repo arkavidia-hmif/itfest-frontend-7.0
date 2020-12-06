f8967d149364daf202ae04730abf201e9c7050b6
Fabian Pranoto <fabian.s.d.p@gmail.com>
2020-12-06 11:38:22 +0700

    edit map hover

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
+import Map from "../components/Map/Map";
 import Layout from "components/commons/Layout";
 
 const Home: React.FC = () => {
-  return <Layout title="Home">rgt</Layout>;
+  return (
+    <Layout title="Home">
+      <Map/>
+    </Layout>
+  );
 };
 
 export default Home;
