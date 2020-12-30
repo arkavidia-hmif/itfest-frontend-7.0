584a57875b99758e2dc5f22687028627760a2cb1
jrandiny <jrandiny@gmail.com>
2020-12-30 22:24:03 +0700

    Add debug info on home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import Layout from "components/commons/Layout";
 
 const Home: React.FC = () => {
-  return <Layout title="Home">rgt</Layout>;
+  return <Layout title="Home">{process.env.API_BASE_URL}</Layout>;
 };
 
 export default Home;
