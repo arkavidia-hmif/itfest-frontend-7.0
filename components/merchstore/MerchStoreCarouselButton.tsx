445933ece7070aa338708cc60f7f618c096359a3
jrandiny <jrandiny@gmail.com>
2021-01-12 15:26:45 +0700

    Add animation on carousel button

--- components/merchstore/MerchStoreCarouselButton.tsx
+++ components/merchstore/MerchStoreCarouselButton.tsx
@@ -4,33 +4,37 @@ const MerchStoreCarouselButton: React.FC<RenderArrowProps> = ({ type, onClick, i
   const buttonImgSrc = `/img/merchstore/${type.toLowerCase()}_button.png`;
 
   return (
-    <div className="next-btn-container">
-      <button onClick={onClick} disabled={isEdge}>
+    <div className="d-flex flex-column justify-content-center next-btn-container">
+      <button onClick={onClick} disabled={isEdge} className="carousel-btn">
         <img
           src={buttonImgSrc}
-          className="next-icon"
+          className="carousel-icon p-2"
         />
       </button>
       <style jsx>{`
-        .next-btn-container {
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-        }
-
-        .next-btn-container button {
+        .carousel-btn {
+          width: 2.5rem;
+          height: 2.5rem;
+          padding: 0;
           background: none;
           border: none;
         }
 
-        .next-btn-container button:focus {
-          outline: none;
-        }
-
-        .next-icon {
-          width: 2rem;
+        .carousel-icon {
+          width: 2.5rem;
+          height: 2.5rem;
           border-radius: 50%;
           box-shadow: 1px 1px 8px rgba(0, 0, 0, 0.25);
+          transition: box-shadow 200ms ease-out;
+          transition: opacity 200ms ease-out;
+        }
+
+        .carousel-btn:disabled .carousel-icon{
+          opacity: 0;
+        }
+
+        .carousel-btn:not([disabled]) .carousel-icon:hover {
+          box-shadow: 5px 5px 8px rgba(0, 0, 0, 0.25);
         }
       `}</style>
     </div>
