950f7b95b4d031af7dd17171c014e3730b3b473c
jrandiny <jrandiny@gmail.com>
2021-01-24 23:30:12 +0700

    Implement basic login register

--- api/auth.ts
+++ api/auth.ts
@@ -1,8 +1,6 @@
 import { AxiosInstance } from "axios";
 import {
   AuthData,
-  EmailResetPasswordStatus,
-  EmailVerifyStatus,
   LoginStatus,
   RegisterStatus,
 } from "interfaces/auth";
@@ -14,7 +12,7 @@ export async function login(
   password: string
 ): Promise<AuthData> {
   try {
-    const response = await axios.post("/auth/login/", {
+    const response = await axios.post("/login", {
       email,
       password,
     });
@@ -22,16 +20,11 @@ export async function login(
     return response.data as AuthData;
   } catch (e) {
     if (e.response) {
-      const errorCode = e.response.data.code;
-      if (errorCode === "login_failed" || errorCode === "unknown_error") {
+      const errorCode = e.response.code;
+      if (errorCode === "user-not-found" || errorCode === "invalid-auth") {
         throw new ApiError<LoginStatus>(
           LoginStatus.INVALID_CREDS,
-          e.response.data.detail
-        );
-      } else if (errorCode === "account_email_not_confirmed") {
-        throw new ApiError<LoginStatus>(
-          LoginStatus.EMAIL_NOT_CONFIRMED,
-          e.response.data.detail
+          "Email dan/atau password salah"
         );
       }
     }
@@ -40,15 +33,13 @@ export async function login(
   }
 }
 
-export async function register(
+export async function registerVisitor(
   axios: AxiosInstance,
-  fullName: string,
   email: string,
-  password: string
+  password: string,
 ): Promise<AuthData> {
   try {
-    const response = await axios.post("/auth/register/", {
-      fullName,
+    const response = await axios.post("/register/visitor/", {
       email,
       password,
     });
@@ -57,76 +48,14 @@ export async function register(
   } catch (e) {
     if (e.response) {
       const errorCode = e.response.data.code;
-      if (errorCode === "registration_failed_email_used") {
+      if (errorCode === "user-exists") {
         throw new ApiError<RegisterStatus>(
-          RegisterStatus.EMAIL_USED,
-          e.response.data.detail
+          RegisterStatus.USER_EXISTS,
+          "Email ini sudah terdaftar"
         );
       }
     }
 
-    throw new ApiError<StandardError>(StandardError.ERROR, e);
-  }
-}
-
-export async function requestResetPassword(
-  axios: AxiosInstance,
-  email: string
-): Promise<void> {
-  try {
-    await axios.post("/auth/password-reset/", {
-      email,
-    });
-  } catch (e) {
-    throw new ApiError<StandardError>(StandardError.ERROR, e);
-  }
-}
-
-export async function resetPassword(
-  axios: AxiosInstance,
-  token: string,
-  newPassword: string
-): Promise<void> {
-  try {
-    await axios.post("/auth/confirm-password-reset/", {
-      token,
-      newPassword,
-    });
-  } catch (e) {
-    if (e.response) {
-      if (
-        e.response.data.code === "invalid_token" ||
-        e.response.data.code === "token_used"
-      ) {
-        throw new ApiError<EmailResetPasswordStatus>(
-          EmailResetPasswordStatus.INVALID_TOKEN,
-          e.response.data.detail
-        );
-      }
-    }
-    throw new ApiError<EmailResetPasswordStatus>(
-      EmailResetPasswordStatus.ERROR,
-      e
-    );
-  }
-}
-
-export async function confirmEmailAddress(
-  axios: AxiosInstance,
-  token: string
-): Promise<void> {
-  try {
-    await axios.post("/auth/confirm-registration/", { token });
-  } catch (e) {
-    if (e.response) {
-      if (e.response.data.code === "invalid_token") {
-        throw new ApiError<EmailVerifyStatus>(
-          EmailVerifyStatus.INVALID_TOKEN,
-          e.response.data.detail
-        );
-      }
-    }
-
-    throw new ApiError<EmailVerifyStatus>(EmailVerifyStatus.ERROR, e);
+    throw new ApiError<StandardError>(StandardError.ERROR, e.message);
   }
 }
--- interfaces/auth.ts
+++ interfaces/auth.ts
@@ -1,39 +1,12 @@
 export interface AuthData {
-  token: string;
-  exp: number;
-  user: UserData;
-}
-
-export interface ProfileData {
-  fullName: string;
-  currentEducation: string | null;
-  institution: string | null;
-  phoneNumber: string | null;
-  birthDate: string | null;
-  address: string | null;
-}
-
-export interface UserData extends ProfileData {
-  dateJoined: string;
-  email: string;
+  jwt: string;
 }
 
 export enum LoginStatus {
-  ERROR,
-  INVALID_CREDS,
-  EMAIL_NOT_CONFIRMED,
+  INVALID_CREDS
 }
 
-export enum EmailResetPasswordStatus {
-  ERROR,
-  INVALID_TOKEN,
-}
-
-export enum EmailVerifyStatus {
-  ERROR,
-  INVALID_TOKEN,
-}
 
 export enum RegisterStatus {
-  EMAIL_USED,
+  USER_EXISTS
 }
