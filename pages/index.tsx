75e7ed2c088b842c36455ec3c8f3b9e5e80265c8
16519111 <gregdimasbaskara@gmail.com>
2021-01-25 14:09:53 +0700

    carousel ready

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,8 @@ import dynamic from "next/dynamic";
 import Layout from "components/commons/Layout";
 import Leaderboard from "components/home/Leaderboard";
 import MapDescription from "components/home/MapDescription";
+import Carousel from "components/Carousel";
+import CarouselItem from "components/CarouselItem";
 
 const Home: React.FC = () => {
   const MapWithNoSSR = dynamic(() => import("components/Map/Map"), {
@@ -10,6 +12,10 @@ const Home: React.FC = () => {
 
   return (
     <Layout title="Home">
+      <Carousel>
+        <CarouselItem textCompany="Dinosaurus" mainText="Lalaland" backgroundImage="img/dino.png"/>
+        <CarouselItem textCompany="Brontosaurus" mainText="Lalaland2"/>
+      </Carousel>
       <MapDescription />
       <MapWithNoSSR />
       <Leaderboard />
