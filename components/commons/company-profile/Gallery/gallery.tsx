96d2b5788ccf1aa50ae1db06398cb12f3f547d6b
Fabian Pranoto <fabian.s.d.p@gmail.com>
2021-01-30 12:23:38 +0700

    add create it photos

--- components/commons/company-profile/Gallery/gallery.tsx
+++ components/commons/company-profile/Gallery/gallery.tsx
@@ -137,7 +137,6 @@ const Gallery: React.FC<Props> = ({items}) => {
       <style jsx>{`
         img {
           height: 100%;
-          width: 100%;
         }
         
         .flex-container {
--- utils/constants/tenants.ts
+++ utils/constants/tenants.ts
@@ -6,10 +6,16 @@ const Tenants: Array<Tenant> = [
     logo: "/img/company-logo/createit.jpg",
     slug: "create-it",
     position: [125,275],  /* Posisi di Map */
-    aboutUs: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut ",
+    aboutUs: "Create It is an EduTech Platform focused on Project-Based and Creative Economy-related curriculum to help final year students, fresh grads, and career-shifters to become creative agile talent to build a professional track in the digital & creative industry. So far, we have 3 major Profession outcomes: Creativepreneur, professional, and Content Creators / Freelance. Let’s Create with Create It!",
     galleryText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut ",
     video: "",
-    gallery: ["/img/company-logo/createit.jpg"]
+    gallery: [
+      "/img/company-gallery/create-it/ict-expo-1.jpg", "/img/company-gallery/create-it/ict-expo-2.jpg", 
+      "/img/company-gallery/create-it/nakama-2020.jpeg", "/img/company-gallery/create-it/nextdev-1.jpeg", 
+      "/img/company-gallery/create-it/nextdev-2.jpeg", "/img/company-gallery/create-it/startup-weekend.jpeg",
+      "/img/company-gallery/create-it/the-greaterhub.PNG", "/img/company-gallery/create-it/ui-studentpreneur.jpeg", 
+      "/img/company-gallery/create-it/wice-2020.png"
+    ]
   },
   {
     name: "Dino",
@@ -19,7 +25,9 @@ const Tenants: Array<Tenant> = [
     aboutUs: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut ",
     galleryText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut ",
     video: "",
-    gallery: ["/img/company-logo/dino.png","/img/company-logo/dino.png", "/img/company-logo/dino.png", "/img/company-logo/dino.png"]
+    gallery: [
+      "/img/company-logo/dino.png","/img/company-logo/dino.png", 
+      "/img/company-logo/dino.png", "/img/company-logo/dino.png"]
   },
 ];
 
