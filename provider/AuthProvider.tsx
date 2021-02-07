41976856e9f09bb9e33d4e57ce2655a35b69570b
jrandiny <jrandiny@gmail.com>
2021-02-07 22:08:56 +0700

    Update provider to include profile

--- provider/AuthProvider.tsx
+++ provider/AuthProvider.tsx
@@ -1,5 +1,5 @@
 import { ReactNode, useEffect, useState } from "react";
-import { AuthData } from "interfaces/auth";
+import { AuthData, VisitorProfileData } from "interfaces/auth";
 import { AuthContext, AuthContextType } from "utils/context/auth";
 
 interface Props {
@@ -9,11 +9,13 @@ interface Props {
 const AuthProvider: React.FC<Props> = ({ children }) => {
   const [authenticated, setAuthenticated] = useState(false);
   const [auth, setAuth] = useState<AuthData>();
+  const [profile, setProfile] = useState<VisitorProfileData>();
   const [loaded, setLoaded] = useState(false);
 
   const authenticatedKey =
     process.env.LOCAL_STORAGE_AUTHENTICATED || "authenticated_dev";
   const authKey = process.env.LOCAL_STORAGE_AUTH || "auth_dev";
+  const profileKey = process.env.LOCAL_STORAGE_PROFILE || "profile_dev";
 
   const setAndSaveAuthenticated = (newValue: boolean) => {
     setAuthenticated(newValue);
@@ -29,11 +31,24 @@ const AuthProvider: React.FC<Props> = ({ children }) => {
     }
   };
 
+  const setAndSaveProfile = (newValue?: VisitorProfileData) => {
+    setProfile(newValue);
+    if (newValue) {
+      localStorage.setItem(profileKey, JSON.stringify(newValue));
+    } else {
+      localStorage.removeItem(profileKey);
+    }
+  };
+
   useEffect(() => {
     setAuthenticated(localStorage.getItem(authenticatedKey) === "true");
     const savedAuth = localStorage.getItem(authKey);
     if (savedAuth) {
       setAuth(JSON.parse(savedAuth));
+    }
+    const savedProfile = localStorage.getItem(profileKey);
+    if (savedProfile) {
+      setProfile(JSON.parse(savedProfile));
     }
     setLoaded(true);
   }, []);
@@ -41,8 +56,10 @@ const AuthProvider: React.FC<Props> = ({ children }) => {
   const authContext: AuthContextType = {
     authenticated,
     auth,
+    profile,
     setAuthenticated: setAndSaveAuthenticated,
     setAuth: setAndSaveAuth,
+    setProfile: setAndSaveProfile
   };
 
   if (loaded || typeof window === "undefined") {
--- utils/context/auth.ts
+++ utils/context/auth.ts
@@ -1,11 +1,13 @@
 import { createContext } from "react";
-import { AuthData } from "interfaces/auth";
+import { AuthData, VisitorProfileData } from "interfaces/auth";
 
 export interface AuthContextType {
   authenticated: boolean;
   auth?: AuthData;
+  profile?: VisitorProfileData;
   setAuthenticated: (newValue: boolean) => void;
-  setAuth: (newValue?: AuthData) => void;
+  setAuth: (newValue: AuthData) => void;
+  setProfile: (newValue: VisitorProfileData) => void;
 }
 
 export const AuthContext = createContext<AuthContextType>(
