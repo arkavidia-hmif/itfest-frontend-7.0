12000a69ec3f3dbf52f3a858ed7dd4b630ee26fc
Joshua <jrandiny@gmail.com>
2021-02-18 20:28:48 +0700

    Fix lint

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,9 +1,9 @@
-import { useContext } from "react";
-import { useRouter } from "next/dist/client/router";
+// import { useContext } from "react";
+// import { useRouter } from "next/dist/client/router";
 import Layout from "components/commons/Layout";
 import ProfileWrapper from "components/profile/ProfileWrapper";
 import { Theme } from "styles/theme";
-import { AuthContext } from "utils/context/auth";
+// import { AuthContext } from "utils/context/auth";
 
 const Home: React.FC = () => {
 //   const authContext = useContext(AuthContext);
