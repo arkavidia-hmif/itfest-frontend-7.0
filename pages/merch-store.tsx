68c41c2f7bc628c5f87aa3b1afe72fe7c317e279
jrandiny <jrandiny@gmail.com>
2021-02-20 00:51:58 +0700

    Remove redundant merch store header

--- pages/merch-store.tsx
+++ pages/merch-store.tsx
@@ -2,9 +2,6 @@ import * as React from "react";
 import MerchStore from "components/merchstore/MerchStore";
 import MerchStoreTop from "components/merchstore/MerchStoreTop";
 import Layout from "components/commons/Layout";
-import ColorfulHeader from "components/ColorfulHeader";
-import { Theme } from "styles/theme";
-import GradientSeparator from "components/auth/GradientSeparator";
 import CheckoutBagProvider from "provider/CheckoutBagProvider";
 
 
@@ -14,8 +11,6 @@ const MerchStoreMain: React.FC = () => {
     <Layout title={title}>
       <CheckoutBagProvider>
         <div className="container">
-          <ColorfulHeader color={Theme.headerColors.pipl} headingLevel={1} size="3rem">{title}</ColorfulHeader>
-          <GradientSeparator />
           <div className="mb-5" />
           <div className="mt-3 mb-5">
             <MerchStoreTop />
