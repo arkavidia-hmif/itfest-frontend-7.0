7d13afb69af326b616221f699b6bc5db83857c64
Jones Napoleon Autumn <13518086@std.stei.itb.ac.id>
2021-02-20 14:27:31 +0700

    lint

--- components/profile/ProfileWrapper.tsx
+++ components/profile/ProfileWrapper.tsx
@@ -10,23 +10,21 @@ import { getProfile, PROFILE_URL } from "api/profile";
 import Alert from "components/commons/Alert";
 import Spinner from "components/commons/Spinner";
 
-
 const ProfileWrapper: React.FC = () => {
   const apiContext = useContext(ApiContext);
 
   const [selection, setSelection] = useState(0);
   const getComponent = () => {
-    if (selection === 0){
+    if (selection === 0) {
       return <PrimaryField />;
-    } 
-    if (selection === 1){
+    }
+    if (selection === 1) {
       return <PersonalField />;
-    } 
+    }
   };
 
-  const { data: profile, error: errorProfile } = useSWR (
-    PROFILE_URL,
-    () => getProfile(apiContext.axios)
+  const { data: profile, error: errorProfile } = useSWR(PROFILE_URL, () =>
+    getProfile(apiContext.axios)
   );
 
   if (errorProfile) return <Alert error="Masalah koneksi" />;
@@ -45,50 +43,47 @@ const ProfileWrapper: React.FC = () => {
           </ColorfulHeader>
         </div>
         <div className="col-1 points mr-1">
-          <h2 id="point">Points<div className="indicator"></div></h2>
+          <h2 id="point">
+            Points<div className="indicator"></div>
+          </h2>
         </div>
         <div className="col-9">
           <h2>{profile?.point}</h2>
         </div>
         <div className="col-lg-4 col-xs-12 mt-4">
-          <ProfileSidebar
-            setSelection={setSelection}
-            selection={selection}
-          />
+          <ProfileSidebar setSelection={setSelection} selection={selection} />
         </div>
-        <div className="col-lg-8 col-xs-12 mt-4">
-          {getComponent()}
-        </div>
+        <div className="col-lg-8 col-xs-12 mt-4">{getComponent()}</div>
       </div>
       <style jsx>{`
-      .main-profile {
-        height: 90vh;
-        background: url(/img/bg-white.png);
-        background-position: center 13em;
-        background-repeat: no-repeat;
-        background-size: 60%;
-      }
-      .points {
-        max-width: 4.5em;
-      }
-      #point {
-        color: #441985;
-      }
-      h2 {
-        color: #441985;
-      }
-      .indicator {
-        position: absolute;
-        width: 100%;
-        height: 0.25em;
-        background: linear-gradient(90deg, #fe789a 0%, #623FA2 100%);
-      }
-      @media screen and (max-width: 991px) {
         .main-profile {
-          background: none;
+          height: 90vh;
+          background: url(/img/bg-white.png);
+          background-position: center 13em;
+          background-repeat: no-repeat;
+          background-size: 60%;
         }
-      }
-    `}</style>
+        .points {
+          max-width: 4.5em;
+        }
+        #point {
+          color: #441985;
+        }
+        h2 {
+          color: #441985;
+        }
+        .indicator {
+          position: absolute;
+          width: 100%;
+          height: 0.25em;
+          background: linear-gradient(90deg, #fe789a 0%, #623fa2 100%);
+        }
+        @media screen and (max-width: 991px) {
+          .main-profile {
+            background: none;
+          }
+        }
+      `}</style>
     </div>
   );
 };
