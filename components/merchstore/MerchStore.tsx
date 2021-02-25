9710527d2f283a0904ebf250b38d9f959965b86f
Nathaniel Jason <nthnieljson@gmail.com>
2021-02-25 18:42:31 +0700

    fix linter

--- components/merchstore/MerchStore.tsx
+++ components/merchstore/MerchStore.tsx
@@ -1,10 +1,10 @@
-import { useState, useEffect, useMemo, useRef } from "react";
+import { useState, useEffect, useMemo} from "react";
+import Carousel from "react-elastic-carousel";
 import MerchStoreCarouselButton from "./MerchStoreCarouselButton";
 import MerchStoreExpanded from "./MerchStoreExpanded";
 import MerchStoreSimple from "./MerchStoreSimple";
 import SnackBar from "./SnackBar";
 import Tenants from "utils/constants/tenants";
-import Carousel from "react-elastic-carousel";
 import { MerchStoreMerchantCarouselBreakPoints } from "utils/constants/merch-store-merchant";
 
 const MerchStore: React.FC = () => {
@@ -51,11 +51,11 @@ const MerchStore: React.FC = () => {
 
                   initialActiveIndex={storeArray.length * 2}
                   onNextStart={() => {
-                    setCurrentPosition(currentPosition + 1)
+                    setCurrentPosition(currentPosition + 1);
                   }}
 
                   onPrevStart={() => {
-                    setCurrentPosition(currentPosition - 1)
+                    setCurrentPosition(currentPosition - 1);
                   }}
 
                   renderPagination={() => <></>}
@@ -65,7 +65,7 @@ const MerchStore: React.FC = () => {
                 >
                   {storeCarouselArray.map((merchant, index) => (
                     <div key={index} className={`merch-store-simple ${index === currentPosition ? "" : "merch-store-simple-minor"
-                      }`}>
+                    }`}>
                       <MerchStoreSimple
                         merchant={merchant}
                         handleMore={handleMore}
