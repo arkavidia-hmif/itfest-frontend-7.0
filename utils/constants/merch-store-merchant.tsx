4558911d69a1c3dc7d5a42c9dbd297b7d56b05d3
jrandiny <jrandiny@gmail.com>
2021-02-27 06:15:07 +0700

    Limit merch store to 2

--- utils/constants/merch-store-merchant.tsx
+++ utils/constants/merch-store-merchant.tsx
@@ -1,5 +1,5 @@
 export const MerchStoreMerchantCarouselBreakPoints = [
   { width: 1, itemsToShow: 1 },
   { width: 592, itemsToShow: 2 },
-  { width: 1000, itemsToShow: 3 },
+  { width: 1000, itemsToShow: 2 },
 ];
